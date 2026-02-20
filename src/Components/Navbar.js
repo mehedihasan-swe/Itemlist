cd61df082f7684d719e62ab946ec55108a4b4226
mehedihasan-swe <mh121499@gmail.com>
2026-02-21 05:07:25 +0600

    [Style]Added Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Navbar() {
+export default function Navbar({ categoryname, data, setallData }) {
+  let [cname,setCname]=useState("");
+
+  const catname = (name) => {
+   
+    const filterdatacat = data.filter(items => items.category === name)
+     setCname(name);
+    setallData(filterdatacat);
+    console.log(filterdatacat);
+
+    if (filterdatacat.length) {
+      setallData(filterdatacat)
+    } else {
+      setallData(data)
+    }
+
+    console.log(cname);
+  }
+
   return (
     <div className='navbar'>
-
-      <div className='item'>
+      <div className='item' onClick={() => catname("")} style={{ backgroundColor: cname === "" ? "rgb(28, 164, 155)" : "" }}>
         All
       </div>
-      <div className='item'>
-        Fruits
-      </div>
-      <div className='item'>
-        Gadgets
-      </div>
-      <div className='item'>
-        Cloth
-      </div>
-
+      {
+        categoryname.map((name, index) => (
+          <div className='item' key={index} onClick={() => catname(name)} style={{ backgroundColor: name === cname ? "rgb(28, 164, 155)" : "" }}>
+            {name}
+          </div>
+        ))
+      }
     </div>
   )
 }
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import Cards from './Cards'
 import jacket from "../assets/jacket.jpg"
@@ -6,55 +6,55 @@ import iphone from "../assets/iphone.png"
 import phone from "../assets/phone.jpg"
 import apple from "../assets/apple.jpg"
 import orange from "../assets/orange.jpg"
+
 export default function Products() {
 
-  const data = [
-
+  const data = [   
     {
       id: 1,
       title: "jacket",
       image: jacket,
       price: "1200",
-      category:"Cloth"
+      category: "Cloth"
     },
     {
-        id: 2,
-        title: "Iphone",
-        image: iphone,
-        price: "81200",
-        category: "Phone"
+      id: 2,
+      title: "Iphone",
+      image: iphone,
+      price: "81200",
+      category: "Phone"
     },
     {
-        id: 3,
-        title: "Iphone 10",
-        image: phone,
-        price: "90000",
-        category: "Phone"
+      id: 3,
+      title: "Iphone 10",
+      image: phone,
+      price: "90000",
+      category: "Phone"
     },
     {
-        id: 4,
-        title: "Apple",
-        image: apple,
-        price: "180",
-        category: "Fruit"
+      id: 4,
+      title: "Apple",
+      image: apple,
+      price: "180",
+      category: "Fruit"
     },
     {
-        id: 5,
-        title: "Orange",
-        image: orange,
-        price: "180",
-        category: "Fruit"
-    },
-
+      id: 5,
+      title: "Orange",
+      image: orange,
+      price: "180",
+      category: "Fruit"
+    }  
   ]
-
+  //Avoid DUplicate value
+let categoryname = [...new Set(data.map( items => items.category ))];
+const [alldata, setallData] = useState(data);
   return (
     <div>
-      <Navbar />
+      <Navbar categoryname={categoryname} data={data} setallData={setallData}/>
       <div className='parentcard'>
-        <Cards data={data}/>
+        <Cards data={alldata} />
       </div>
-   
     </div>
   )
 }
